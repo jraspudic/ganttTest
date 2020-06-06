43bc4814e0798924e80cc115851ad9fe1910f80e
Jurica Raspudić <jurica.raspudic@gmail.com>
2020-06-06 18:56:36 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,115 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { DataManager, WebApiAdaptor, UrlAdaptor } from "@syncfusion/ej2-data";
+import { EditSettingsModel, ToolbarItem } from "@syncfusion/ej2-angular-gantt";
+import { HttpClient } from "@angular/common/http";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'ganttDemo';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  public data: DataManager;
+  public taskSettings: object;
+  public editSettings: EditSettingsModel;
+  public toolbar: ToolbarItem[];
+  public columns: object[];
+  public timelineSettings: object;
+  public gridLines: string;
+  public labelSettings: object;
+  public projectStartDate: Date;
+  public projectEndDate: Date;
+  public ngOnInit(): void {
+    this.data = new DataManager({
+      url:
+        "https://ej2services.syncfusion.com/production/web-services/api/GanttData",
+      insertUrl:
+        "https://ej2services.syncfusion.com/production/web-services/api/GanttData",
+      adaptor: new UrlAdaptor(), //WebApiAdaptor //crudUrl
+      crossDomain: true,
+    });
+    this.taskSettings = {
+      id: "TaskId",
+      name: "TaskName",
+      startDate: "StartDate",
+      duration: "Duration",
+      progress: "Progress",
+      dependency: "Predecessor",
+      child: "SubTasks",
+    };
+    this.columns = [
+      {
+        field: "TaskName",
+        headerText: "Task Name",
+        width: "250",
+        clipMode: "EllipsisWithTooltip",
+      },
+      { field: "StartDate" },
+      { field: "Duration" },
+    ];
+    this.timelineSettings = {
+      timelineUnitSize: 50,
+      topTier: {
+        unit: "Month",
+        format: "MMM dd, y",
+      },
+      bottomTier: {
+        unit: "Day",
+        formatter: (date) => {
+          let month: number = date.getMonth();
+          if (month == 1) {
+            return "";
+          } else {
+            let presentDate = new Date(
+              date.getFullYear(),
+              date.getMonth(),
+              date.getDate()
+            );
+            var start = new Date(presentDate.getFullYear(), 0, 0);
+            var diff = Number(presentDate) - Number(start);
+            var oneDay = 1000 * 60 * 60 * 24;
+            var day = Math.floor(diff / oneDay);
+            return "day " + (day - 59);
+          }
+        },
+      },
+    };
+    this.gridLines = "Both";
+    this.labelSettings = {
+      leftLabel: "TaskName",
+    };
+    this.projectStartDate = new Date("02/24/2019");
+    this.projectEndDate = new Date("06/10/2019");
+
+    this.editSettings = {
+      allowAdding: true,
+      allowEditing: true,
+      allowDeleting: true,
+      allowTaskbarEditing: true,
+      showDeleteConfirmDialog: true,
+    };
+    this.toolbar = [
+      "Add",
+      "Edit",
+      "Update",
+      "Delete",
+      "Cancel",
+      "ExpandAll",
+      "CollapseAll",
+    ];
+  }
+
+  sendPost() {
+    this.http.post("nekiUrl/api", "dataString").subscribe(
+      (data) => {
+        console.log("vraceni data");
+      },
+      (err) => {
+        console.log("Error se desio");
+        console.log(err);
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { GanttModule } from "@syncfusion/ej2-angular-gantt";
+import { AppComponent } from "./app.component";
+import {
+  ToolbarService,
+  EditService,
+  SelectionService,
+} from "@syncfusion/ej2-angular-gantt";
+import { HttpClientModule } from "@angular/common/http";
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, GanttModule, HttpClientModule],
+  providers: [ToolbarService, EditService, SelectionService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
